59f87a5f71190636abe655b58d4b3ac0cfa9ff85
Abhiraj Gupta <abhiraj675gupta01@gmail.com>
2026-03-13 17:39:27 +0530

    server message fix

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,10 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 
+app.get("/", (req, res) => {
+  res.send("Amazon Clone Backend API Running 🚀");
+});
+
 app.get("/health", (req, res) => {
   res.json({ ok: true });
 });
